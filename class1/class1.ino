e91786a00c62940737eee4b067f95925df845794
Ashraful Haque <ashrafmoin0@gmail.com>
2021-08-12 23:02:36 +0600

    Day 1

---
+++ class1/class1.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);//[pin no.,status-output]
+pinMode(12,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(5,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13,HIGH);//[pin no.,value-high/(5 volt
+//delay(1000);
+//digitalWrite(13,LOW);
+//delay(1000);
+
+digitalWrite(12,HIGH);
+digitalWrite(11,HIGH);
+digitalWrite(10,HIGH);
+digitalWrite(9,HIGH);
+digitalWrite(8,HIGH);
+digitalWrite(7,HIGH);
+digitalWrite(6,HIGH);
+digitalWrite(5,HIGH);
+}
